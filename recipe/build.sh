554c10418addda52568de5b853d4caf5cd08f7a4
Jung Sangwoo <holyavarice@gmail.com>
2024-07-24 05:31:34 +0900

    git clone macos sdk

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,22 +4,10 @@ OS=$(uname)
 
 # macOS인 경우에만 실행
 if [ "$OS" == "Darwin" ]; then
-    XCODE_DOWNLOAD_URL="https://developer.apple.com/services-account/download?path=/Developer_Tools/Xcode_15/Xcode_15.xip"
-    XCODE_FILE_NAME="Xcode_15.xip"
-    XCODE_APP_NAME="Xcode.app"
-    DOWNLOAD_DIR="$HOME/Downloads"
-    mkdir -p "$DOWNLOAD_DIR"
-    curl -s -L $XCODE_DOWNLOAD_URL -o "$DOWNLOAD_DIR/$XCODE_FILE_NAME"
-    unzip "$DOWNLOAD_DIR/$XCODE_FILE_NAME" -d "$DOWNLOAD_DIR"
-    mv "$DOWNLOAD_DIR/$XCODE_APP_NAME" /Applications/
-    ls "/Applications/$XCODE_APP_NAME"
-    ls "/Applications/$XCODE_APP_NAME/Contents"
-    ls "/Applications/$XCODE_APP_NAME/Contents/Developer"
-    ls "/Applications/$XCODE_APP_NAME/Contents/Developer/Platforms"
-    ls "/Applications/$XCODE_APP_NAME/Contents/Developer/Platforms/MacOSX.platform"
-    ls "/Applications/$XCODE_APP_NAME/Contents/Developer/Platforms/MacOSX.platform/Developer"
-    ls "/Applications/$XCODE_APP_NAME/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs"
-
+    git clone https://github.com/alexey-lysiuk/macos-sdk.git
+    mv ./macos-sdk/MacOSX14.5.sdk /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/
+    export CONDA_BUILD_SYSROOT='/Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.5.sdk'
+    export MACOSX_DEPLOYMENT_TARGET='14.5'
 fi
 
 ${PYTHON} -m pip install . -vv
