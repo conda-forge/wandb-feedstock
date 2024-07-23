c0474ea4e82c23d285dc8c014882bd3bf95c5d0f
Jung Sangwoo <holyavarice@gmail.com>
2024-07-24 05:50:24 +0900

    export some variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,10 @@ if [ "$OS" == "Darwin" ]; then
     mv ./macos-sdk/MacOSX14.5.sdk /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/
     export CONDA_BUILD_SYSROOT='/Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.5.sdk'
     export MACOSX_DEPLOYMENT_TARGET='14.5'
+    export MACOSX_SDK_VERSION='14.5'
+    export c_stdlib_version='14.5'
+    plutil -replace MinimumSDKVersion -string 14.5 /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Info.plist
+    plutil -replace DTSDKName -string macosx14.5internal /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Info.plist
 fi
 
 ${PYTHON} -m pip install . -vv
