89fbfa62fa14ec667ffde9f2c9852ca16d53f39b
Jung Sangwoo <holyavarice@gmail.com>
2024-07-27 00:09:05 +0900

    change build requirements

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,18 +4,8 @@ OS=$(uname)
 
 # macOS인 경우에만 실행
 if [ "$OS" == "Darwin" ]; then
-    pwd
-    ls -al
-    xcode-select -p
-    git clone https://github.com/alexey-lysiuk/macos-sdk.git
-    mv ./macos-sdk/MacOSX14.5.sdk /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/
-    export CONDA_BUILD_SYSROOT='/Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.5.sdk'
-    export SDKROOT='/Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.5.sdk'
-    export MACOSX_DEPLOYMENT_TARGET='14.5'
-    export MACOSX_SDK_VERSION='14.5'
-    export c_stdlib_version='14.5'
-    plutil -replace MinimumSDKVersion -string 14.5 /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Info.plist
-    plutil -replace DTSDKName -string macosx14.5internal /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Info.plist
+    sed -i '' 's|// swift-tools-version: 5.9|// swift-tools-version: 5.7.1|' apple_stats/Package.swift
+    sed -i '' 's|\.macOS(.v10_14)|\.macOS(.v10_13)|' apple_stats/Package.swift
 fi
 
 ${PYTHON} -m pip install . -vv
