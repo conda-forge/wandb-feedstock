4e64d32c371108e68d3fa255345a96ebe2deb760
Timofey Peshin <timoffex@gmail.com>
2024-10-18 13:56:14 -0700

    fix bld.bat as well

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,13 @@
 @echo on
 
-PUSHD nvidia_gpu_stats
+PUSHD gpu_stats
 cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml || goto :error
 POPD
 
 %PYTHON% -m pip install --no-deps --no-build-isolation -vv .
 
 PUSHD core
+del LICENSE
 copy ../LICENSE . 
 go-licenses save . --save_path="./license-files" || goto :error
 POPD
