51d5ca067677610f985d59f5f515477eda0c34d1
Timofey Peshin <timoffex@gmail.com>
2024-10-18 12:51:22 -0700

    cp LICENSE to core/ again

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,13 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-pushd gpu_stats 
+pushd gpu_stats
 cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
 popd
 
 ${PYTHON} -m pip install --no-deps --no-build-isolation -vv .
 
-pushd core 
+pushd core
+cp ../LICENSE LICENSE
 go-licenses save . --save_path="./license-files/"
 popd
