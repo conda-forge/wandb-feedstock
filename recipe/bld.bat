7f8d0430820e619c6fd8a8227af32adc7e60de39
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2026-04-20 12:19:25 +0200

    Add `leet` and update build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,22 @@
 @echo on
 
-PUSHD gpu_stats
-cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml || goto :error
+PUSHD xpu
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY_XPU.yml || goto :error
+POPD
+
+PUSHD parquet-rust-wrapper
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY_PARQUET.yml || goto :error
+POPD
+
+PUSHD wandb\vendor\wandb_orjson
+cargo-bundle-licenses --format yaml --output ..\..\..\THIRDPARTY_ORJSON.yml || goto :error
 POPD
 
 %PYTHON% -m pip install --no-deps --no-build-isolation -vv .
 
 PUSHD core
 del LICENSE
-copy ../LICENSE . 
+copy ..\LICENSE .
 go-licenses save . --save_path="./license-files" || goto :error
 POPD
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,17 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-pushd gpu_stats
-cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
+# Bundle Rust licenses for xpu (formerly gpu_stats), parquet-rust-wrapper, and vendored orjson
+pushd xpu
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY_XPU.yml
+popd
+
+pushd parquet-rust-wrapper
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY_PARQUET.yml
+popd
+
+pushd wandb/vendor/wandb_orjson
+cargo-bundle-licenses --format yaml --output ../../../THIRDPARTY_ORJSON.yml
 popd
 
 ${PYTHON} -m pip install --no-deps --no-build-isolation -vv .
